359ec97eb58481d4d2949c6f652ec53524e1fe66
Jennifer <j22036384@student.newinti.edu.my>
2025-10-12 16:52:31 +0800

    with about me section

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def index():
 
     return render_template('index.html', projects=projects)
 
+@app.route('/about_me')
+def about_me():
+    with open('static/data/experience.json') as project:
+        experience_projects = json.load(project)
+
+    with open('static/data/skills.json') as skills_project:
+        skills = json.load(skills_project)
+
+    left_column = skills[::2]
+    right_column = skills[1::2]
+    return render_template("about_me.html", experience_projects=experience_projects, left_column=left_column, right_column=right_column)
+
 if __name__ == '__main__':
     app.run(debug=True)
